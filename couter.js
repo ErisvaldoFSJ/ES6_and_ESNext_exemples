2ef54561b892bcf066cf88e6a17f215b9a75e4c4
erisvaldo <erisvaldo.fsj@gmail.com>
2018-03-08 14:24:45 -0300

    new couter

--- couter.js
+++ couter.js
@@ -1,29 +1,48 @@
 
-let count = 0
-const addOne = ()=>{
-    count++
-    renderCouterApp()
+
+class Counter extends React.Component{
+    constructor(props){
+        super(props)
+        this.handleAddOne = this.handleAddOne.bind(this)
+        this.handleMinusOne = this.handleMinusOne.bind(this)
+        this.handleReset = this.handleReset.bind(this)
+        this.state = {
+            count: 0,
+            name: 'julie'
+        }
+    }
+    handleAddOne(){
+        this.setState((prevState)=>{
+            return {
+                count:prevState.count+1
+            }
+        })
+    }
+    handleMinusOne(){
+        this.setState((prevState)=>{
+            return {
+                count: prevState.count -1
+            }
+        })
+    }
+    handleReset(){
+        this.setState(()=>{
+            return{
+                count: 0
+            }
+        })
+    }
+    render(){
+        return(
+            <div>
+                {this.state.name}
+                <h1>Count:{this.state.count} </h1>
+                <button onClick={this.handleAddOne}>+1</button>
+                <button onClick={this.handleMinusOne}>-1</button>
+                <button onClick={this.handleReset}>reset</button>
+            </div>
+        )
+    }
 }
 
-const minusOne= ()=>{
-    count--
-    renderCouterApp()
-}
-const reset = () =>{
-    count = 0
-    renderCouterApp()
-}
-
-
-const renderCouterApp = ()=>{
-    const templateTwo = (
-        <div>
-            <h1>Cout: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>reset</button>
-        </div>
-    )
-    ReactDOM.render(templateTwo, appRoot)
-}
-renderCouterApp()
+ReactDOM.render(<Counter/>, document.getElementById('app'))
